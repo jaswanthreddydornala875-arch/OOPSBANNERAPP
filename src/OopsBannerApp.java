e44727fa63f931c1e130a4fca1980625ea77f25b
Dornala Sri Dharma Jaswanth Reddy <jaswanthreddydornala875@gmail.com>
2026-02-22 00:31:25 +0530

    UC3

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,12 +1,11 @@
 public class OopsBannerApp{
-   public static void main(String[] args){
-        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"    *****");
-        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" **      ");
-        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
-        System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "+" **      ");
-        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"  ***    ");
-        System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"      ** ");
-        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"**    ** ");
-        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+"*****    ");
+    public static void main(String[] args){
+        System.out.println(String.join(" " ,"   ***   ","   ***   "," ******  ","   *****"));
+        System.out.println(String.join(" "," **   ** "," **   ** ","**    ** "," **      "));
+        System.out.println(String.join(" ","**     **","**     **","**    ** ","**       "));
+        System.out.println(String.join(" ","**     **","**     **","******   ","   ***   "));
+        System.out.println(String.join(" ","**     **","**     **","**       ","      ** "));
+        System.out.println(String.join(" "," **   ** "," **   ** ","**       ","**     **"));
+        System.out.println(String.join(" ","   ***   ","   ***   ","**       "," *****   "));
     }
 }
